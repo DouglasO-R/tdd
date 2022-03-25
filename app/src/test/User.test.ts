ad259fccfa1957fcfb4c9c334358cf92852d9a4c
douglasor <douglasor@outlook.com>
2022-03-25 15:59:45 -0300

    TDD - GREEN - Create User

--- app/src/test/User.test.ts
+++ app/src/test/User.test.ts
@@ -1,3 +1,30 @@
+export class User{
+
+    id?:string;
+	name:string; 
+	username: string; 
+	todos?:[];
+
+    constructor({name,username,id}:User){
+        this.id = !id ? "uuid-v4" : id;
+        this.name = name;
+        this.username = username;
+        this.todos = [];
+    }
+}
+
+type CreateUserDTO = {
+    name:string;
+    username:string
+};
+export class CreateOneUSer{
+
+    async with({name,username}:CreateUserDTO):Promise<User>{
+        const user = await new User({name,username});
+        return user;
+    }
+}
+
 describe("User", () => {
 
     test("should be create one user", async () => {
@@ -7,8 +34,9 @@ describe("User", () => {
         };
 
         const createOneUser = new CreateOneUSer();
-        const createdUser = createOneUser.with(userToBeCreated);
+        const createdUser = await createOneUser.with(userToBeCreated);
 
-        expect(createdUser).toEqual(userToBeCreated)
+        expect(createdUser).toMatchObject(userToBeCreated);
+        expect(createdUser.id).toBe("uuid-v4");
     });
 });
